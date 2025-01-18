bf2c0d82dd91d3dad3c1e3d96b9ca87b9c4e6454
Ricman <ricardofreccero@hotmail.com>
2025-01-17 21:02:41 -0300

    La sidebar esta preparada para ser usada como un control general

--- src/sidebar_animada.py
+++ src/sidebar_animada.py
@@ -1,88 +1,35 @@
 import flet
 import time
+from math import pi
 
 
-class Sidebar(flet.Container):
-    def __init__(self, ):
+class SidebarDestination(flet.Container):
+    def __init__(self, icono: flet.Icons, texto: str):
         super().__init__(
-            bgcolor=flet.Colors.SURFACE,
-            width=200,
-            alignment=flet.alignment.center,
-            padding=flet.Padding(top=15, left=10, right=10, bottom=0),
-            animate=flet.animation.Animation(
-                duration=300,
-                curve=flet.AnimationCurve.DECELERATE
-            ),
-            content=flet.Column(
-                horizontal_alignment=flet.CrossAxisAlignment.CENTER,
+            key="",
+            width=180,
+            height=45,
+            border_radius=10,
+            on_hover=self.hover,
+            content=flet.Row(
                 controls=[
-                    self.datos_usuarios(
-                        iniciales="RF",
-                        nombre="Ricardo Freccero",
-                        descripcion="Ingeniero en Sistemas"
-                    ),
                     flet.IconButton(
-                        icon=flet.Icons.ARROW_BACK_IOS_ROUNDED,
-                        on_click=self.animar_sidebar,
-                        # bgcolor=flet.Colors.WHITE10,
-                        # icon_color=flet.Colors.WHITE54,
+                        icon=icono,
+                        icon_size=18,
+                        style=flet.ButtonStyle(
+                            shape=flet.RoundedRectangleBorder(radius=7),
+                            overlay_color=flet.Colors.TRANSPARENT,
+                        ),
                     ),
-                    flet.Divider(
-                        height=5,
-                        color=flet.Colors.TRANSPARENT,
-                    ),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.SEARCH,
-                        texto="Search",
-                    ),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.DASHBOARD_ROUNDED,
-                        texto="Dashboard",
-                    ),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.BAR_CHART,
-                        texto="Revenue",
-                    ),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.NOTIFICATIONS,
-                        texto="Norifications",
-                    ),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.PIE_CHART_ROUNDED,
-                        texto="Analytics",
-                    ),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.FAVORITE_ROUNDED,
-                        texto="Likes",
-                    ),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.WALLET_ROUNDED,
-                        texto="Wallet",
-                    ),
-                    flet.Divider(height=5, color=flet.Colors.WHITE54),
-                    self.icono_sidebar(
-                        nombre_icono=flet.Icons.LOGOUT_ROUNDED,
-                        texto="Logout",
-                    ),
+                    flet.Text(
+                        value=texto,
+                        size=11,
+                        opacity=1,
+                        animate_opacity=200,
+                    )
                 ]
             )
         )
-
-    def seleccionar(self, e):
-        # Le quitamos el color a todos
-        for destination in self.content.controls[3:]:
-            if isinstance(destination, flet.Container):
-                destination.key = ""
-                destination.bgcolor = None
-                destination.content.controls[0].icon_color = flet.Colors.PRIMARY
-                destination.content.controls[1].color = flet.Colors.ON_SURFACE_VARIANT
-
-        # Le damos el color al elemento seleccionado
-        e.control.key = "seleccionado"
-        e.control.bgcolor = flet.Colors.SECONDARY_CONTAINER
-        e.control.content.controls[0].icon_color = flet.Colors.ON_SECONDARY_CONTAINER
-        e.control.content.controls[1].color = flet.Colors.ON_SECONDARY_CONTAINER
-        self.update()
 
     def hover(self, e):
         if e.control.key != "seleccionado":
@@ -96,21 +43,84 @@ class Sidebar(flet.Container):
                 e.control.content.controls[1].color = flet.Colors.ON_SURFACE
             e.control.update()
 
+
+class Sidebar(flet.Container):
+    def __init__(
+            self,
+            iniciales,
+            nombre,
+            descripcion,
+            destinations: [SidebarDestination],
+            log_out: SidebarDestination | None = None
+    ):
+        self.destinations = [destination for destination in destinations]
+        for destination in self.destinations:
+            destination.on_tap_down = self.seleccionar
+        self.titulo = self.datos_usuarios(
+            iniciales=iniciales,
+            nombre=nombre,
+            descripcion=descripcion
+        )
+        super().__init__(
+            bgcolor=flet.Colors.SURFACE,
+            width=self.titulo.width,
+            alignment=flet.alignment.center,
+            padding=flet.Padding(top=15, left=10, right=10, bottom=0),
+            animate=flet.animation.Animation(
+                duration=300,
+                curve=flet.AnimationCurve.DECELERATE
+            ),
+            content=flet.Column(
+                horizontal_alignment=flet.CrossAxisAlignment.CENTER,
+                controls=[
+                    self.titulo,
+                    flet.IconButton(
+                        icon=flet.Icons.ARROW_BACK_IOS_ROUNDED,
+                        on_click=self.animar_sidebar,
+                        rotate=flet.transform.Rotate(0, alignment=flet.alignment.center),
+                        animate_rotation=flet.animation.Animation(300, flet.AnimationCurve.DECELERATE),
+                    ),
+                    flet.Divider(
+                        height=5,
+                        color=flet.Colors.TRANSPARENT,
+                    ),
+                    *self.destinations,
+                    (flet.Divider(height=5), log_out) if log_out else flet.Container()
+                ]
+            )
+        )
+
+    def seleccionar(self, e):
+        # Le quitamos el color a todos
+        for destination in self.destinations:
+            if isinstance(destination, flet.Container):
+                destination.key = ""
+                destination.bgcolor = None
+                destination.content.controls[0].icon_color = flet.Colors.PRIMARY
+                destination.content.controls[1].color = flet.Colors.ON_SURFACE_VARIANT
+
+        # Le damos el color al elemento seleccionado
+        e.control.key = "seleccionado"
+        e.control.bgcolor = flet.Colors.SECONDARY_CONTAINER
+        e.control.content.controls[0].icon_color = flet.Colors.ON_SECONDARY_CONTAINER
+        e.control.content.controls[1].color = flet.Colors.ON_SECONDARY_CONTAINER
+        self.update()
+
     def datos_usuarios(self, iniciales, nombre, descripcion):
         return flet.Container(
-            key="",
             content=flet.Row(
                 controls=[
                     flet.Container(
                         width=42,
                         height=42,
-                        bgcolor=flet.Colors.BLUE_GREY_900,
+                        bgcolor=flet.Colors.TERTIARY,
                         alignment=flet.alignment.center,
                         border_radius=8,
                         content=flet.Text(
                             value=iniciales,
                             size=20,
                             weight=flet.FontWeight.BOLD,
+                            color=flet.Colors.ON_TERTIARY,
                         )
                     ),
                     flet.Column(
@@ -128,7 +138,6 @@ class Sidebar(flet.Container):
                                 value=descripcion,
                                 size=9,
                                 weight=flet.FontWeight.W_400,
-                                color=flet.Colors.WHITE54,
                                 opacity=1,
                                 animate_opacity=200,
                             ),
@@ -138,58 +147,29 @@ class Sidebar(flet.Container):
             )
         )
 
-    def icono_sidebar(self, nombre_icono, texto):
-        return flet.Container(
-            width=180,
-            height=45,
-            border_radius=10,
-            on_hover=self.hover,
-            on_tap_down=self.seleccionar,
-            content=flet.Row(
-                controls=[
-                    flet.IconButton(
-                        icon=nombre_icono,
-                        icon_size=18,
-                        style=flet.ButtonStyle(
-                            shape=flet.RoundedRectangleBorder(radius=7),
-                            overlay_color=flet.Colors.TRANSPARENT,
-                        ),
-                    ),
-                    flet.Text(
-                        value=texto,
-                        size=11,
-                        opacity=1,
-                        animate_opacity=200,
-                    )
-                ]
-            )
-        )
-
     def animar_sidebar(self, e):
         items_titulo = self.content.controls[0].content.controls[1].controls
-        iconos_sidebar = self.content.controls[3:]
         if self.width != 62:
             for item in items_titulo:
                 item.opacity = 0
-            for item in iconos_sidebar:
+            for item in self.destinations:
                 if isinstance(item, flet.Container):
                     item.content.controls[1].opacity = 0
+            e.control.rotate.angle += pi
             self.update()
             time.sleep(0.2)
             self.width = 62
-            e.control.icon = flet.Icons.ARROW_FORWARD_IOS_ROUNDED
-            self.update()
         else:
-            self.width = 200
+            self.width = self.titulo.width
+            e.control.rotate.angle -= pi
             self.update()
             time.sleep(0.1)
             for item in items_titulo:
                 item.opacity = 1
-            for item in iconos_sidebar:
+            for item in self.destinations:
                 if isinstance(item, flet.Container):
                     item.content.controls[1].opacity = 1
-            e.control.icon = flet.Icons.ARROW_BACK_IOS_ROUNDED
-            self.update()
+        self.update()
 
 
 def main(page: flet.Page):
@@ -271,7 +251,32 @@ def main(page: flet.Page):
         page.update()
 
     page.theme_mode = "dark"
-    page.add(Sidebar(), flet.IconButton(icon=flet.Icons.MODE_NIGHT, on_click=change))
+    page.add(
+        Sidebar(
+            iniciales="ALB",
+            nombre="Albano Construcciones S.R.L.",
+            descripcion="Construcción",
+            destinations=[
+                SidebarDestination(
+                    icono=flet.Icons.SEARCH,
+                    texto="Search",
+                ),
+                SidebarDestination(
+                    icono=flet.Icons.SEARCH,
+                    texto="Search",
+                ),
+                SidebarDestination(
+                    icono=flet.Icons.SEARCH,
+                    texto="Search",
+                ),
+                SidebarDestination(
+                    icono=flet.Icons.SEARCH,
+                    texto="Search",
+                ),
+            ]
+        ),
+        flet.IconButton(icon=flet.Icons.MODE_NIGHT, on_click=change)
+    )
 
 
 flet.app(main)
